968fd2573a4d27f00ceb8aa3f95a52952bfaae85
yuwenjun32 <46049426+yuwenjun32@users.noreply.github.com>
2019-01-03 22:12:30 +0800

    建立数据库

---
+++ app/src/main/java/com/example/lovem/addressbook/DatabaseHelper.java
@@ -0,0 +1,58 @@
+package com.example.lovem.addressbook;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DB_NAME="MyRelation.db";
+    private static final String TABLE_NAME = "relation";
+    private static final String CREATE_TABLE = "create table relation(_id integer primary key autoincrement," +
+            "name text," +
+            "tel text," +
+            "groupName text);";
+    private SQLiteDatabase db;
+
+    DatabaseHelper(Context context){
+        super(context,DB_NAME,null,2);
+    }
+
+    public void insert(ContentValues values){
+        SQLiteDatabase db=getWritableDatabase();
+        db.insert(TABLE_NAME,null,values);
+        db.close();
+    }
+
+    public void del(int id){
+        if (db==null)
+            db=getWritableDatabase();
+        db.delete(TABLE_NAME,"_id=?",new String[]{String.valueOf(id)});
+
+    }
+
+    public Cursor query(){
+        SQLiteDatabase db=getWritableDatabase();
+        Cursor cursor=db.query(TABLE_NAME,null,null,null,null,null,null);
+        return cursor;
+    }
+
+    public void close(){
+        if (db!=null)
+            db.close();
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        this.db=db;
+        db.execSQL(CREATE_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
