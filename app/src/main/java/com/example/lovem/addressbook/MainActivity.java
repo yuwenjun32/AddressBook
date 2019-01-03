6f57cb73f6312713b0fdec79c7a836a32b384dcc
yuwenjun32 <46049426+yuwenjun32@users.noreply.github.com>
2019-01-03 17:02:09 +0800

    动态欢迎界面的实现

--- app/src/main/java/com/example/lovem/addressbook/MainActivity.java
+++ app/src/main/java/com/example/lovem/addressbook/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.lovem.addressbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView iv_main_welcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iv_main_welcome=(ImageView)findViewById(R.id.iv_main_welcome);
+        Animation anim=AnimationUtils.loadAnimation(this,R.anim.set);
+        iv_main_welcome.startAnimation(anim);
+
 
     }
 }
