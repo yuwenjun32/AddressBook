07b14262226cf21d64f44053dbc18794a8f354b8
yuwenjun32 <46049426+yuwenjun32@users.noreply.github.com>
2019-01-04 11:34:54 +0800

    建立主界面

---
+++ app/src/main/java/com/example/lovem/addressbook/main_interface_Activity.java
@@ -0,0 +1,13 @@
+package com.example.lovem.addressbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class main_interface_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_interface_);
+    }
+}
