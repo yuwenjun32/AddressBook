4e91d75b3961b0ace6de3b15636b991592b7ce25
xiezhaoyu015 <46081028+xiezhaoyu015@users.noreply.github.com>
2019-01-04 16:06:29 +0800

    编辑新增联系人界面

--- app/src/main/java/com/example/lovem/addressbook/AddRelationActivity.java
+++ app/src/main/java/com/example/lovem/addressbook/AddRelationActivity.java
@@ -1,13 +1,49 @@
 package com.example.lovem.addressbook;
 
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class AddRelationActivity extends AppCompatActivity {
+    private EditText addName,addTel;
+    private Spinner addGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_relation);
+        addName=(EditText)findViewById(R.id.addName);
+        addTel=(EditText)findViewById(R.id.addTel);
+        addGroup=(Spinner)findViewById(R.id.addGroup);
+    }
+    public void save(View view){
+        final ContentValues values=new ContentValues();
+        values.put("name",addName.getText().toString());
+        values.put("tel",addTel.getText().toString());
+        values.put("groupName",addGroup.getSelectedItem().toString());
+        final DatabaseHelper dbHelper=new DatabaseHelper(getApplicationContext());
+        final AlertDialog.Builder adBuilder=new AlertDialog.Builder(this);
+        adBuilder.setMessage("确认保存记录吗？").setPositiveButton("确认", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dbHelper.insert(values);
+                Intent intent=getIntent();
+                setResult(0x111,intent);
+                AddRelationActivity.this.finish();
+            }
+        }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        AlertDialog alertDialog=adBuilder.create();
+        alertDialog.show();
     }
 }
